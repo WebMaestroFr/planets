4c9dc283f37e196a6b2686bbdcd98484f6e0c7a5
Etienne <etienne@ori.co>
2020-10-11 16:31:00 +0200

    Added Delaunay Tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,97 @@
-import React, { FC, useRef, useState } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
-import { Mesh } from "three";
+import { geoDelaunay } from "d3-geo-voronoi";
+import React, { FC, useEffect, useRef, useState } from "react";
+import { Canvas, useFrame, useUpdate } from "react-three-fiber";
+import seedrandom from "seedrandom";
+import { ConeGeometry, MathUtils, Mesh } from "three";
 
-const Box: FC<{ position: [number, number, number] }> = (props) => {
+const RADIUS = 2;
+const RESOLUTION = 64;
+
+type Coordinates = [number, number];
+
+const getRandomCoordinates = (length: number, seed?: string): Coordinates[] => {
+  const random = seedrandom(seed);
+  const getCoordinates = (): Coordinates => [
+    random() * 360 - 180,
+    random() * 180 - 90,
+  ];
+  return Array.from({ length }, getCoordinates);
+};
+
+const getPolygons = (coordinates: Coordinates[]): Coordinates[][] => {
+  const delaunay = geoDelaunay(coordinates);
+  return delaunay.polygons.map((polygon: number[]) =>
+    polygon.map((c) => delaunay.centers[c])
+  );
+};
+
+const TileGeometry: FC<{ polygon: Coordinates[] }> = ({
+  polygon,
+  ...props
+}) => {
+  const ref = useUpdate<ConeGeometry>((geometry) => {
+    const origin = geometry.vertices[0];
+    const center = geometry.vertices[polygon.length + 1];
+    origin.set(0, 0, 0);
+    center.set(0, 0, 0);
+    for (let index = 0; index < polygon.length; index++) {
+      const [lon, lat] = polygon[index];
+      const vertex = geometry.vertices[index + 1];
+      const phi = MathUtils.degToRad(90 - lat);
+      const theta = MathUtils.degToRad(lon);
+      vertex.setFromSphericalCoords(RADIUS, phi, theta);
+      center.add(vertex);
+    }
+    center.setLength(RADIUS);
+    geometry.verticesNeedUpdate = true;
+    geometry.elementsNeedUpdate = true;
+    geometry.uvsNeedUpdate = true;
+    geometry.normalsNeedUpdate = true;
+    geometry.colorsNeedUpdate = true;
+  }, []);
+
+  return <coneGeometry args={[1, 1, polygon.length]} {...props} ref={ref} />;
+};
+
+const TileMesh: FC<{ index: number; polygon: Coordinates[] }> = ({
+  index,
+  polygon,
+  ...props
+}) => {
   // This reference will give us direct access to the mesh
-  const mesh = useRef<Mesh>(null);
-
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
+  const ref = useRef<Mesh>(null);
 
   // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => {
-    if (mesh && mesh.current) {
-      mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+    if (ref && ref.current) {
+      ref.current.rotation.x += 0.02;
     }
   });
 
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}
-    >
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    <mesh {...props} ref={ref}>
+      <TileGeometry polygon={polygon} />
+      <meshStandardMaterial color={Math.random() * 0xffffff} />
     </mesh>
   );
 };
 
 const App: FC = () => {
+  const [tiles, setTiles] = useState<Coordinates[][]>([]);
+
+  useEffect(() => {
+    const points = getRandomCoordinates(RESOLUTION);
+    const polygons = getPolygons(points);
+    setTiles(polygons);
+  }, []);
+
   return (
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      {tiles.map((polygon, index) => (
+        <TileMesh key={index} index={index} polygon={polygon} />
+      ))}
     </Canvas>
   );
 };
